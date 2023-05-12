ad54f4c9b66c22f3e5176c8d98ba930af5daa927
sven-pke <sven-perschke@gmx.de>
2023-05-12 11:00:56 +0200

    // module: division a / b |  test:

--- script_01.js
+++ script_01.js
@@ -10,32 +10,50 @@
 4. Ausgabe in Konsole : 
 */
 
+// Konstanten
+const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
+const ERROR_STR_GEN = "Irgendetwas ging schief!"
+
+
+
+
+
 // module: addition a + b |  test:
-output(add(2,2));
-output(add(2,-2));
-output(add(2,0));
+// output(add(2,2));
+// output(add(2,-2));
+// output(add(2,0));
 function add(a, b) {
 	return a + b;
 }
 
 // module: subtraction a - b |  test:
-output(substract(3,2));
-output(substract(3,-2));
-output(substract(3,0));
+// output(substract(3,2));
+// output(substract(3,-2));
+// output(substract(3,0));
 function substract(a, b) {
 	return a - b;
 }
 
-function add(a, b) {
-	let c = a + b;
-	return c;
+// module: multiplication a * b |  test:
+// output(multiply(3,2));
+// output(multiply(3,-2));
+// output(multiply(3,0));
+function muliply(a, b) {
+	return a * b;
 }
 
-function add(a, b) {
-	let c = a + b;
-	return c;
+// module: division a / b |  test:
+// output(divide(4,2));
+// output(divide(3,2));
+// output(divide(3,-2));
+// output(divide(3,0));
+function divide(a,b) {
+	
+if (b == 0) {
+	return ERROR_STR_DIV; // Ausnahme + Abbruch
+} 
+	return a / b; // Regel
 }
-
 
 
 
