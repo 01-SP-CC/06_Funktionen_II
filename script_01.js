1b469654954f4c12824aa83ecb6e53d8a3b596fb
sven-pke <sven-perschke@gmx.de>
2023-05-12 11:47:27 +0200

    // module: calculator | tests:

--- script_01.js
+++ script_01.js
@@ -5,18 +5,14 @@
 0. a+b | a-b | a*b | a/b  --> Ergebnis c 
 
 1. Dateneingabe + -überprüfung : 
-2. Auswahl Rechenart : 
-3. Fkt. Grundrechenarten : 
+2. Auswahl Rechenart : check
+3. Fkt. Grundrechenarten : check
 4. Ausgabe in Konsole : 
 */
 
 // Konstanten
 const ERROR_STR_DIV = "Division durch 0 nicht möglich!";
 const ERROR_STR_GEN = "Irgendetwas ging schief!"
-
-
-
-
 
 // module: addition a + b |  test:
 // output(add(2,2));
@@ -48,19 +44,42 @@ function muliply(a, b) {
 // output(divide(3,-2));
 // output(divide(3,0));
 function divide(a,b) {
-	
-if (b == 0) {
-	return ERROR_STR_DIV; // Ausnahme + Abbruch
-} 
-	return a / b; // Regel
+	if (b == 0) {
+		return ERROR_STR_DIV; // Ausnahme + Abbruch
+	}
+		return a / b; // Regel
 }
 
 
+// module: calculator | tests:
+// agreement : "+","-","*",":","/"
+// output(calculator(3,2,"+"));
+// output(calculator(3,2,"-"));
+// output(calculator(3,2,"*"));
+// output(calculator(3,2,":"));
+// output(calculator(3,2,"/"));
+// output(calculator(3,0,"/"));
+// output(calculator(3,2,"#?!"));
 
 
+function calculator(a,b,op) {
 
-
-
+switch (op) {
+	case "+": return add(a,b);
+		// return add
+	case "-": return substract(a,b);
+		// return sub
+	case "*": return muliply(a,b);
+		// return mul
+	case "/": return divide(a,b);
+	case ":": return divide(a,b);
+		// return add
+		
+	default:
+		return ERROR_STR_GEN;
+}
+	
+}
 
 
 // module: output | test:
