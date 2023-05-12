9542a279e3331ce77290c465d2d4079fb5eee094
sven-pke <sven-perschke@gmx.de>
2023-05-12 10:18:28 +0200

    module: subtraction a - b

--- script_01.js
+++ script_01.js
@@ -11,82 +11,49 @@
 */
 
 // module: addition a + b |  test:
-
-// output(add(2,2));
-// output(add(2,-2));
-// output(add(2,0));
-
-// function add(a, b) {
-// 	let c = a + b;
-// 	return c;
-	
-// }
-
-
-
-// // module: output | test:
-// // output("hello");
-// // output(2);
-// function output(outputData) {
-// 	console.log(outputData);
-// }
-
-
-
-
-
-
-// // Dateneingabe und Überprüfung
-
-let a = prompt("Gib den ersten Wert ein:");
-let operation = prompt("Wähle eine Rechenart (+, -, *, /):");
-let b = prompt("Gib den zweiten Wert ein:");
-
-if(isNaN(a) || isNaN(b)){
-console.log("Fehler: Ungültige Eingabe!");
-} else {
-
-
-// Funktionen für Grundrechenarten
-
-function add(a,b){
-return parseFloat(a) + parseFloat(b);
+output(add(2,2));
+output(add(2,-2));
+output(add(2,0));
+function add(a, b) {
+	return a + b;
 }
 
-function subtract(a,b){
-return parseFloat(a) - parseFloat(b);
+// module: subtraction a - b |  test:
+output(substract(3,2));
+output(substract(3,-2));
+output(substract(3,0));
+function substract(a, b) {
+	return a - b;
 }
 
-function multiply(a,b){
-return parseFloat(a) * parseFloat(b);
+function add(a, b) {
+	let c = a + b;
+	return c;
 }
 
-function divide(a,b){
-return parseFloat(a) / parseFloat(b);
+function add(a, b) {
+	let c = a + b;
+	return c;
 }
 
-//Berechnung des Ergebnisses und Ausgabe in der Konsole
 
-let result;
-switch(operation){
-case "+":
-result = add(a,b);
-break;
-case "-":
-result = subtract(a,b);
-break;
-case "*":
-result = multiply(a,b);
-break;
-case "/":
-result = divide(a,b);
-break;
-default:
-console.log("Fehler: Ungültige Rechenart!");
-break;
+
+
+
+
+
+
+
+
+// module: output | test:
+// output("hello");
+// output(2);
+function output(outputData) {
+	console.log(outputData);
 }
 
-if(result !== undefined){
-console.log("Ergebnis: " + result);
-}
-}
+
+
+
+
+
